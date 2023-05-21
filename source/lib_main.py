666cdbdb7a779111133943b57612c436b0d83480
Marius Stuebs <mqs@stuebs.tech>
2023-05-21 16:15:22 +0700

    Improve leetcode API calls

--- source/lib_main.py
+++ source/lib_main.py
@@ -68,7 +68,17 @@ def replace_grid_field(grid, x, y, char):
     grid[y] = grid[y][:x] + char + grid[y][x + 1:]
             
 def main(challenge_id=-1):
+<<<<<<< Updated upstream
     if int(challenge_id) not in [1514, 864, 1970, 2305]:
+=======
+    if int(challenge_id) not in [
+        1514, 
+        864, 
+        1970, 
+        2305,
+        1601,
+        ]:
+>>>>>>> Stashed changes
         print("Required argument challenge_id: must be a value of [1514, 864, 1970, 2305]")
         return -1
         
@@ -173,5 +183,32 @@ def main(challenge_id=-1):
                 cells.append([y+1,x+1])
         tests += "\n" + "\n".join([row.__str__(), col.__str__(), cells.__str__()])
 
+<<<<<<< Updated upstream
     date = time.time()
     write_file(f"testcase_{challenge_id}_{int(date)}.txt", tests)
+=======
+    if int(challenge_id) == 1601:
+        tests = ""
+        for test in range(8):
+            building = 20
+            request_count = 16
+            requests = []
+            for i in range(request_count):
+                requests.append([random.randint(0,building-1),random.randint(0,building-1)])
+            tests += "\n" + "\n".join([building.__str__(), requests.__str__()])
+            
+        building = 20
+        requests = [[0,1],[1,2],[2,3],[3,0],[0,1],[1,2],[2,3],[3,0],[3,4],[4,3],[3,4],[4,3],[3,4],[4,3]]
+        tests += "\n" + "\n".join([building.__str__(), requests.__str__()])
+        
+        building = 4
+        for orig in range(0,4):
+            for dest in range(0,4):
+                requests.append(orig, dest)
+        tests += "\n" + "\n".join([building.__str__(), requests.__str__()])
+
+            
+
+    date = time.time()
+    write_file(f"testcase_{challenge_id}_{int(date)}.txt", tests)
+>>>>>>> Stashed changes
