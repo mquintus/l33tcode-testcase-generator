337fcfb5628ef5321da1d01eb721bacb9f4b04f3
Marius Stuebs <mqs@stuebs.tech>
2023-09-20 21:41:35 -0700

    Generate testcases for challenge 4 median-of-two-sorted-arrays

--- source/Generator4.py
+++ source/Generator4.py
@@ -5,18 +5,35 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 0
+    max_num = 1000
+    minval = -1e6
+    maxval = 1e6
 
+    m = min_num
+    n = 100
+    nums1 = sorted([random.randint(minval, maxval) for _ in range(n)])
+    nums2 = sorted([random.randint(minval, maxval) for _ in range(m)])
+    tests.append((nums1.__str__() + "\n" + nums2.__str__()).replace(' ', ''))
+
+    m = 99
     n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    nums1 = sorted([random.randint(minval, maxval) for _ in range(n)])
+    nums2 = sorted([random.randint(minval, maxval) for _ in range(m)])
+    tests.append((nums1.__str__() + "\n" + nums2.__str__()).replace(' ', ''))
+
+    m = max_num // 2
+    n = max_num - 1
+    nums1 = sorted([random.randint(minval, maxval) for _ in range(n)])
+    nums2 = sorted([random.randint(minval, maxval) for _ in range(m)])
+    tests.append((nums1.__str__() + "\n" + nums2.__str__()).replace(' ', ''))
+
+    for _ in range(5):
+        m = random.randint(1, max_num)
+        n = random.randint(1, max_num)
+        nums1 = sorted([random.randint(0, 1000) for _ in range(n)])
+        nums2 = sorted([random.randint(999, 2000) for _ in range(m)])
+        tests.append((nums1.__str__() + "\n" + nums2.__str__()).replace(' ', ''))
+
     return '''
 '''.join(tests)
