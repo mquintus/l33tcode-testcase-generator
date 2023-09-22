061a8ea43e36ba83b4e758b20a69c4c42c23cafc
Marius Stuebs <mqs@stuebs.tech>
2023-09-21 22:53:06 -0700

    Update testcases for 392_Is_Subsequence

--- source/Generator392.py
+++ source/Generator392.py
@@ -5,18 +5,42 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_length_s = 0
+    max_length_s = 100
+    min_length_t = 0
+    max_length_t = 10000
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    letters = "abcdefghijklmnopqrstuvxyz"
+
+    def get_random_string(length):
+        selection = '"' + "".join([random.choice(letters) for _ in range(length)]) + '"'
+        return selection
+
+    m = min_length_s
+    n = min_length_t
+    test = get_random_string(m) + "\n" + get_random_string(n)
+    tests.append(test)
+
+    m = min_length_s
+    n = max_length_t
+    test = get_random_string(m) + "\n" + get_random_string(n)
+    tests.append(test)
+
+    m = max_length_s
+    n = min_length_t
+    test = get_random_string(m) + "\n" + get_random_string(n)
+    tests.append(test)
+
+    m = max_length_s
+    n = max_length_t
+    test = '"' + "a"*m + '"' +"\n" + '"' + "a"*(m-1) + "b"*(n-m) + '"'
+    tests.append(test)
+
+    for _ in range(4):
+        m = max_length_s
+        n = max_length_t
+        test = get_random_string(m) + "\n" + get_random_string(n)
+        tests.append(test)
+
     return '''
 '''.join(tests)
