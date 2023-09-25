9e2e8749238778a1ba9edb95d760a79dde412682
Marius Stuebs <mqs@stuebs.tech>
2023-09-24 17:50:14 -0700

    source/Generator389.py

--- source/Generator389.py
+++ source/Generator389.py
@@ -5,18 +5,22 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_letters = 0
+    max_letters = 1000
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    for length in range(min_letters, max_letters  + 1, (max_letters - min_letters) // 8):
+        if length == (max_letters - min_letters) // 4:
+            # Edge case: t begins with a letter that didn't appear in s
+            s = ["a", "b", "c"]
+            t = ["z", "a", "b", "c"]
+        else:
+            s = [random.choice(letters) for _ in range(length)]
+            t = [*s]
+            t.append(random.choice(letters))
+            random.shuffle(t)
+        test = '"' + ''.join(s) + '"' + '\n' + '"' + ''.join(t) + '"'
+        tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
