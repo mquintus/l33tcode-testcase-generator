c49523a890764f039be64b39d84d704865dd3158
Marius Stuebs <mqs@stuebs.tech>
2023-09-25 21:58:41 -0700

    Update source/Generator316.py

--- source/Generator316.py
+++ source/Generator316.py
@@ -5,18 +5,41 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 1000
+    letters = "abcdefghijklmnopqrstuvwxyz"
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for _ in range(2):
+        s = "".join([random.choice(letters) for i in range(300)]).__str__()
+        tests.append('"' + s + '"')
+
+    for _ in range(1):
+        s = "".join([random.choice(letters) for i in range(max_num)]).__str__()
+        tests.append('"' + s + '"')
+
+    s = "a"
+    c = 0
+    for l in letters[::-1]:
+        c += 100
+        s += l * 100
+        if c >= max_num:
+            break
+    tests.append('"' + s + '"')
+
+    s = letters * (max_num // len(letters))
+    tests.append('"' + s + '"')
+
+    s = letters[::-1] * (max_num // len(letters))
+    tests.append('"' + s + '"')
+
+    s = "g"
+    tests.append('"' + s + '"')
+
+    s = ""
+    for l in letters[::-1]:
+        s += l
+    for l in letters:
+        s += l
+    tests.append('"' + s + '"')
+
     return '''
 '''.join(tests)
