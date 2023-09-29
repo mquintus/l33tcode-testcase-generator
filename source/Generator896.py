a296c5041ec7707732e5017960f6bc9170ea10ef
Marius Stuebs <mqs@stuebs.tech>
2023-09-28 20:24:22 -0700

    source/Generator896.py

--- source/Generator896.py
+++ source/Generator896.py
@@ -5,18 +5,42 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 100000
+    minval = -100000
+    maxval = 100000
+
+    n = 100
+    test = [i for i in range(n)]
+    tests.append(test.__str__().replace(' ', ''))
+
+    n = 99
+    test = [n-i-1 for i in range(n)]
+    tests.append(test.__str__().replace(' ', ''))
+
+    n = 50
+    test = [i * 2 + 1 for i in range(n)]
+    test2 = [i * 2 for i in range(n)]
+    test.extend(test2)
+    tests.append(test.__str__().replace(' ', ''))
 
     n = min_num
     test = [random.randint(minval, maxval) for _ in range(n)]
     tests.append(test.__str__().replace(' ', ''))
-    
+
     n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
+    test = [random.randint(minval, maxval) for _ in range(n)]
     tests.append(test.__str__().replace(' ', ''))
-    
+
+    n = min_num
+    test = [i for i in range(n)]
+    test.append(n - 1)
+    tests.append(test.__str__().replace(' ', ''))
+
+    n = max_num - 2
+    test = [i for i in range(n)]
+    test.append(n - 1)
+    tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
