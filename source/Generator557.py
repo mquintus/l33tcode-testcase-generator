a25894f7ab7a6360bf0651c68c9e96806f90e491
Marius Stuebs <mqs@stuebs.tech>
2023-09-30 19:22:13 -0700

    source/Generator557.py

--- source/Generator557.py
+++ source/Generator557.py
@@ -5,18 +5,21 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_letters = 1
+    max_letters = 50000
+    letters = [chr(x) for x in range(ord("A"), ord("z") + 1)]
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for length in [min_letters, 2, 3, 3, 3, 20, 24, max_letters - 1]:
+        s = ''
+        remaining_length = length
+        while remaining_length > 0:
+            wordlength = random.randint(1, remaining_length)
+            if len(s) > 0:
+                s += " "
+            s += "".join([random.choice(letters) for _ in range(wordlength)])
+            remaining_length = length - len(s)
+        test = '"' + s + '"'
+        tests.append(test.__str__().replace('\\', ''))
+
     return '''
 '''.join(tests)
