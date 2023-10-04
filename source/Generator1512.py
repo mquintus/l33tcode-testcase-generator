3fe0fe63329cfc6ddbcee1623c220079564f8787
Marius Stuebs <mqs@stuebs.tech>
2023-10-04 09:19:58 -0700

    1512_Number_of_Good_Pairs.txt

--- source/Generator1512.py
+++ source/Generator1512.py
@@ -5,18 +5,48 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 100
+    minval = 1
+    maxval = 100
 
     n = min_num
     test = [random.randint(minval, maxval) for _ in range(n)]
     tests.append(test.__str__().replace(' ', ''))
-    
+
     n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
+    test = [random.randint(minval, maxval) for _ in range(n)]
     tests.append(test.__str__().replace(' ', ''))
-    
+
+    # all the same
+    match = random.randint(minval, maxval)
+    test = [match] * max_num
+    tests.append(test.__str__().replace(' ', ''))
+
+    # all the max
+    match = maxval
+    test = [match] * max_num
+    tests.append(test.__str__().replace(' ', ''))
+
+    # only pairs
+    test = []
+    for _ in range(max_num // 2):
+        match = random.randint(minval, maxval)
+        test.extend([match] * 2)
+    tests.append(test.__str__().replace(' ', ''))
+
+    # in reverse
+    test = test[::-1]
+    tests.append(test.__str__().replace(' ', ''))
+
+    # in shuffled
+    random.shuffle(test)
+    tests.append(test.__str__().replace(' ', ''))
+
+    # all different
+    test = [i for i in range(minval, maxval + 1)]
+    tests.append(test.__str__().replace(' ', ''))
+
+
     return '''
 '''.join(tests)
