39d9fffd46e4836f2e736d43048ec21105c137b3
Marius Stuebs <mqs@stuebs.tech>
2023-10-13 22:30:52 -0700

    source/Generator2742.py

--- source/Generator2742.py
+++ source/Generator2742.py
@@ -5,18 +5,20 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_time = 1
+    max_time = 500
+    min_cost = 1
+    max_cost = 10**6
+    min_length = 1
+    max_length = 500
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in n]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_length, 2, 3, 10, 10, 20, 100, max_length]:
+        cost = [random.randint(min_cost, max_cost) for _ in range(n)]
+        time = [random.randint(min_time, n) for _ in range(n)]
+        tests.append(
+            cost.__str__().replace(' ', '')
+            + "\n" + time.__str__().replace(' ', '')
+            )
+
     return '''
 '''.join(tests)
