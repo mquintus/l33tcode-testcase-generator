f9494f4dcf398bd7aafc2023a132ca7fbc9f4826
Marius Stuebs <mqs@stuebs.tech>
2023-10-18 21:36:51 -0700

    844_Backspace_String_Compare

--- source/Generator844.py
+++ source/Generator844.py
@@ -5,18 +5,41 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 200
+    letters = "#abcdefghijklmnopqrstuvwxyz"
+    letters_pure = "abcdefghijklmnopqrstuvwxyz"
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
+    s = "".join([random.choice(letters) for i in range(min_num)]).__str__()
+    t = s
+    test = f'"{s}"\n"{t}"'
     tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
+
+    s = "".join([random.choice(letters) for i in range(max_num)]).__str__()
+    t = s
+    test = f'"{s}"\n"{t}"'
     tests.append(test.__str__().replace(' ', ''))
-    
+
+    s = "#" * max_num
+    t = "".join([random.choice(letters) + '#' for i in range(max_num // 2)]).__str__()
+    test = f'"{s}"\n"{t}"'
+    tests.append(test.__str__().replace(' ', ''))
+
+    for _ in range(3):
+        s = "".join([random.choice(letters) for i in range(33)]).__str__()
+        t = "".join([random.choice(letters) for i in range(33)]).__str__() + "#" * 33 + s
+        test = f'"{s}"\n"{t}"'
+        tests.append(test.__str__().replace(' ', ''))
+
+    s = "".join([random.choice(letters) for i in range(66)]).__str__()
+    t = s + "".join([random.choice(letters_pure) for i in range(66)]).__str__() + "#" * 66
+    test = f'"{s}"\n"{t}"'
+    tests.append(test.__str__().replace(' ', ''))
+
+    s = "".join([random.choice(letters) for i in range(max_num)]).__str__()
+    t = "".join([random.choice(letters) for i in range(max_num)]).__str__()
+    test = f'"{s}"\n"{t}"'
+    tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
