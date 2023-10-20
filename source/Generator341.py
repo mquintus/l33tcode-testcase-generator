87e06f4d479b12849b3be2719e5b0daced223c1a
Marius Stuebs <mqs@stuebs.tech>
2023-10-19 21:38:19 -0700

    generated/341_Flatten_Nested_List_Iterator.txt

--- source/Generator341.py
+++ source/Generator341.py
@@ -5,18 +5,36 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 499
+    minval = -100
+    maxval = 100
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    directions = ['deeper', 'deeper', 'return', 'here']
+
+
+    def create_nested_list(element_count, depth):
+        if element_count == 0:
+            return [], 0
+
+        myList = []
+        while element_count > 0:
+            direction = random.choice(directions)
+            if direction == 'deeper':
+                nestedList, element_count = create_nested_list(element_count - 1, depth + 1)
+                myList.append(nestedList)
+            elif direction == 'return' and depth > 0:
+                break
+            elif direction == 'here':
+                item = random.randint(minval, maxval)
+                myList.append(item)
+                element_count -= 1
+
+        return myList, element_count
+
+    for n in [min_num, 10, 20, 30, 50, 100, max_num, max_num]:
+        test, element_count = create_nested_list(n, 0)
+        tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
