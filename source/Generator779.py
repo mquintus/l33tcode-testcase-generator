b065275e8af2d4175b7a1adedafa56b07d2e9ab7
Marius Stuebs <mqs@stuebs.tech>
2023-10-25 11:26:31 -0700

    generated/779_K-th_Symbol_in_Grammar.txt

--- source/Generator779.py
+++ source/Generator779.py
@@ -5,18 +5,17 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    i = 8
+    n = 30
+    for pot in [random.randint(1,3),random.randint(10,15),random.randint(25,28)]:
+        for k in range(2**pot-0-pot, 2**pot+3-pot):
+            test = f"{n}\n{k}"
+            tests.append(test.__str__().replace(' ', ''))
+            i -= 1
+            if i == 0:
+                break
+        if i == 0:
+            break
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
     return '''
 '''.join(tests)
