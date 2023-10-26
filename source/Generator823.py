1d0af9db50863190b94e430150f6ec196842b925
Marius Stuebs <mqs@stuebs.tech>
2023-10-26 11:56:42 -0700

    2023-10-26 generated/823_Binary_Trees_With_Factors.txt

--- source/Generator823.py
+++ source/Generator823.py
@@ -5,18 +5,22 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 1000
+    minval = 2
+    maxval = 10**9
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
+    all_numbers = range(minval, maxval)
+
+    test = [i for i in range(2, 1002)]
     tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+
+    for n in [min_num, 2, 10, 20, max_num // 2, max_num // 2, max_num // 2, max_num]:
+        test = set([2,3,4,5,6,7,8,10,12,random.choice(all_numbers)])
+        while len(test) < n:
+            new_product = max(minval, (random.choice(list(test)) * random.choice(list(test))) % maxval)
+            test.add(new_product)
+        tests.append(list(test)[:n].__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
