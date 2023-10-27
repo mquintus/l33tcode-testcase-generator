3980d098f22f5a87d187c03b22d3cd07d50bfc3c
Marius Stuebs <mqs@stuebs.tech>
2023-10-27 11:26:21 -0700

    2023-10-27 5_Longest_Palindromic_Substring.txt

--- source/Generator5.py
+++ source/Generator5.py
@@ -5,18 +5,31 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 1000
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    alphabet = list('abcdefghijklmnopqrstuvwxzy0123456789')
+
+    test = [random.choice(alphabet) + random.choice(alphabet)]
+    tests.append('"' + "".join(test) + '"')
+
+    for n in [min_num, max_num]:
+        test = [random.choice(alphabet) for _ in range(n)]
+        tests.append('"' + "".join(test) + '"')
+
+        test = [random.choice(alphabet) * 2 for _ in range(max(1, n//2))]
+        tests.append('"' + "".join(test) + '"')
+
+    char = random.choice(alphabet)
+    test = char * (max_num // 2 - 1) + '65' + random.choice(alphabet) + char * (max_num // 2 - 2)
+    tests.append('"' + "".join(test) + '"')
+
+    char = random.choice(alphabet)
+    test = char * (max_num // 2) + 'a' + char * (max_num // 2 - 1)
+    tests.append('"' + "".join(test) + '"')
+
+    test = [random.choice(alphabet) * (max_num // 5 - random.randint(1,5)) for _ in range(5)]
+    tests.append('"' + "".join(test) + '"')
+
     return '''
 '''.join(tests)
