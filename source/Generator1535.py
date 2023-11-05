327f374a78a41a51706a6539ca235495b4a70057
Marius Stuebs <mqs@stuebs.tech>
2023-11-04 17:53:13 -0700

    2023-11-04 1535_Find_the_Winner_of_an_Array_Game

--- source/Generator1535.py
+++ source/Generator1535.py
@@ -1,4 +1,4 @@
-import random
+from .helpers import helpers
 
 '''
 1535 - Find the Winner of an Array Game
@@ -6,17 +6,19 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**5
+    minval = 1
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    min_k = 1
+    max_k = 10**9
+
+    # Test cases
+    for k in [min_k, max_k]:
+        for n in [min_num, 20, 100, max_num]:
+            unique_numbers = helpers.get_unique_numbers(n, minval)
+            test = f"{unique_numbers}\n{k}"
+            tests.append(test.replace(' ', ''))
+
+
     return '''
 '''.join(tests)
