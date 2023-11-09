33e8930785ecf21c9d42d7e9c20423137a81661d
Marius Stuebs <mqs@stuebs.tech>
2023-11-09 12:51:18 -0800

    2023-11-09 1759_Count_Number_of_Homogenous_Substrings

--- source/Generator1759.py
+++ source/Generator1759.py
@@ -5,18 +5,30 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+
+    letters = "abcdefghijklmnopqrstuvwxyz"
 
     n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    test = "".join([random.choice(letters) for _ in range(n)])
+    tests.append(f'"{test}"')
+
+    test = ""
+    i = random.randint(0,25)
+    for sublength in range(random.randint(15, 25), 100):
+        letter = (i + sublength) % 26
+        test += letters[letter] * sublength
+    tests.append(f'"{test}"')
+
+
+    for n in [2, 100, 10000]: #, max_num]:
+        for repeat in [True, False]:
+            if repeat:
+                test = "".join([random.choice(letters)] * n)
+            else:
+                test = "".join([random.choice(letters) for _ in range(n)])
+            tests.append(f'"{test}"')
+
     return '''
 '''.join(tests)
