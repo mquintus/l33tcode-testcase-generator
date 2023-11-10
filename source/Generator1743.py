e0110782955b54d5503f679fe43d4d1fb829c618
Marius Stuebs <mqs@stuebs.tech>
2023-11-09 17:38:44 -0800

    2023-11-10 #1743. Restore the Array From Adjacent Pairs

--- source/Generator1743.py
+++ source/Generator1743.py
@@ -6,17 +6,16 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**5
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 3, 10, 100, 1000, 10000, max_num]:
+        minval = -n // 2
+        maxval = n // 2
+        random_adder = random.randint(0, n - maxval)
+        all_numbers = list(range(random_adder + minval, random_adder + maxval))
+        random.shuffle(all_numbers)
+        pairs = [[all_numbers[i], all_numbers[i-1]] for i in range(1, len(all_numbers))]
+        tests.append(pairs.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
