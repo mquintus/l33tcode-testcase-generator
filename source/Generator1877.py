7b26fb1c0e4dfc419a0443f7da0fdd0c94a7ad3f
Marius Stuebs <mqs@stuebs.tech>
2023-11-16 21:28:49 -0800

    2023-11-17 #1877. Minimize Maximum Pair Sum in Array

--- source/Generator1877.py
+++ source/Generator1877.py
@@ -6,17 +6,13 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**5
+    minval = 1
+    maxval = 10**5
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 10, 20, 100, max_num, max_num]:
+        test = [random.randint(minval, maxval) for _ in range(n)]
+        tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
