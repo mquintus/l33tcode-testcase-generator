57a15d18bec2b288e6e5d26e44dfd6f8a33033ac
Marius Stuebs <mqs@stuebs.tech>
2023-11-18 12:15:58 -0800

    2023-11-18 #1838. Frequency of the Most Frequent Element

--- source/Generator1838.py
+++ source/Generator1838.py
@@ -5,18 +5,41 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+    minval = 1
+    maxval = 10**5
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    # stepsize: First 2 elements not included
+    # stepsize: Last 2 elements not included
+    k = random.randint(20,70)
+    test = [random.randint(1, 100),
+            random.randint(500, 600),
+            *([random.randint(1000, 1000)] * 2),
+            random.randint(10000, 13000),
+            random.randint(20000, 23000),
+            ]
+    test[3] += random.randint(10, 20)
+    tests.append(test.__str__().replace(' ', '') + "\n" + str(k))
+
+
+    for n in [min_num,2,3,1000,max_num]:
+        k = random.randint(1, max_num)
+        test = [random.randint(minval, maxval) for _ in range(n)]
+        tests.append(test.__str__().replace(' ', '') + "\n" + str(k))
+
+    # sequence all the same
+    k = 1
+    test = [random.randint(minval, maxval)] * n
+    test[3] += 1
+    test[99997] += 1
+    tests.append(test.__str__().replace(' ', '') + "\n" + str(k))
+
+    # sequence all different but reachable as much as possible
+    k = max_num
+    test = [i for i in range(1, n)]
+    random.shuffle(test)
+    tests.append(test.__str__().replace(' ', '') + "\n" + str(k))
+
     return '''
 '''.join(tests)
