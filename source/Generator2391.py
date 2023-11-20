f290dd7315c782679edcdf4bbb4ef0826ece4987
Marius Stuebs <mqs@stuebs.tech>
2023-11-19 19:53:57 -0800

    2023-11-20 #2391. Minimum Amount of Time to Collect Garbage

--- source/Generator2391.py
+++ source/Generator2391.py
@@ -6,17 +6,21 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**5
+    min_garbage = 1
+    max_garbage = 10
+    garbage_type = 'GPM'
+    min_travel = 1
+    max_travel = 100
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 3, 4, 10, 10, 10, 100, max_num]:
+        garbage = []
+        for stop in range(n):
+            number_of_garbage_at_stop = random.randint(min_garbage, max_garbage)
+            garbage_at_stop = "".join([random.choice(garbage_type) for i in range(number_of_garbage_at_stop)])
+            garbage.append(f'"{garbage_at_stop}"')
+        travel = [random.randint(min_travel, max_travel) for i in range(n-1)]
+        tests.append(garbage.__str__().replace(' ', '').replace("'", '') + "\n" + travel.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
