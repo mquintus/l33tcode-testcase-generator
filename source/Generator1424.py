39d736b48ad65f90e7b348d96670eb85b07e26b8
Marius Stuebs <mqs@stuebs.tech>
2023-11-21 21:19:00 -0800

    2023-11-22 #1424. Diagonal Traverse II

--- source/Generator1424.py
+++ source/Generator1424.py
@@ -5,18 +5,33 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
+    min_rows = 1
+    max_rows = 10**4
+    min_cols = 1
+    max_cols = 10**4
+    minval = 1
     maxval = 1000
+    max_cells = 10**5 // 2
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    softmax = random.randint(10, 30)
+
+    testcases = [
+        [min_cols],
+        [softmax for i in range(min_rows)],
+        [1 for _ in range(softmax)],
+        list(range(1, softmax)),
+        list(range(softmax, 0, -1)),
+        [random.randint(min_cols, softmax) for _ in range(softmax)],
+        [max_cols, max_cols // random.randint(20,50), max_cols // random.randint(2,5)],
+        [random.randint(1,3) for _ in range(max_rows)]
+    ]
+
+    for rows in testcases:
+        test = []
+        for cols in rows:
+            test.append([random.randint(minval, min(i+100, maxval)) for i in range(cols)])
+        tests.append(test.__str__().replace(' ', ''))
+
+
     return '''
 '''.join(tests)
