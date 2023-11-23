e2a82ced3251cbb80d80f3eef1002bf2fd21bc36
Marius Stuebs <mqs@stuebs.tech>
2023-11-22 17:46:24 -0800

    2023-11-23 #1630. Arithmetic Subarrays

--- source/Generator1630.py
+++ source/Generator1630.py
@@ -6,17 +6,29 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 500
+    #min_m = 1
+    #max_m = 500
+    minval = -100
+    maxval = 100
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, max_num]:
+        for strategy in ['random', 'sequentially', 'identical']:
+            m = n
+            if strategy == 'random':
+                test = [random.randint(minval, maxval) for _ in range(n)]
+            elif strategy == 'sequentially':
+                rand = random.randint(2, 100)
+                test = [minval + rand*i + random.randint(0,1) for i in range(n)]
+            elif strategy == 'identical':
+                test = [random.randint(minval, maxval)] * n
+            l = []
+            r = []
+            for i in range(m):
+                l.append(random.randint(0, n - 2))
+                r.append(random.randint(l[-1] + 1, min(n-1, l[-1] + 2 + i)))
+            tests.append(test.__str__().replace(' ', '') + "\n" + l.__str__().replace(' ', '') + "\n" + r.__str__().replace(' ', ''))
+
+
     return '''
 '''.join(tests)
