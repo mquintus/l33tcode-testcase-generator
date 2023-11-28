b01d4d3455e2b8d8acb5f1cd092a60bcab4548bf
Marius Stuebs <mqs@stuebs.tech>
2023-11-28 14:12:08 -0800

    2023-11-29 #2147. Number of Ways to Divide a Long Corridor

--- source/Generator2147.py
+++ source/Generator2147.py
@@ -5,18 +5,16 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+    alphabet = 'SP'
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 100, 100, max_num]:
+        test = [random.choice(alphabet) for _ in range(n)]
+        tests.append('"' + "".join(test) + '"')
+
+        test = [random.choice(alphabet)] * n
+        tests.append('"' + "".join(test) + '"')
+
     return '''
 '''.join(tests)
