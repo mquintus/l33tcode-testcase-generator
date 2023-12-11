86bc04682137c976473f1fd7c555069102de8dba
Marius Stuebs <mqs@stuebs.tech>
2023-12-10 21:12:31 -0800

    2023-12-11 #1287. Element Appearing More Than 25% In Sorted Array

--- source/Generator1287.py
+++ source/Generator1287.py
@@ -5,18 +5,33 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
+    min_num = 1
     max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    minval = 1
+    maxval = 10**4 * 2
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 3, 5, 100, 100, 100, max_num]:
+        test = []
+        first_iteration = True
+        used_numbers = set()
+        some_number = random.randint(minval,maxval)
+        while len(test) < n:
+            while some_number in used_numbers:
+                some_number = random.randint(minval,maxval)
+            if first_iteration:
+                first_iteration = False
+                quarter = (n // 4) + 1
+                while quarter < (n / 4):
+                    quarter += 1
+                if n <= 3:
+                    quarter = n
+            elif not first_iteration:
+                quarter = (n // 4)
+                quarter = min(n - len(test), random.randint(1, quarter))
+            test += [some_number] * quarter
+            used_numbers.add(some_number)
+        test.sort()
+        tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
