1caaa1f6bbef0338cef1f4d8c7b181230c015c89
Marius Stuebs <mqs@stuebs.tech>
2023-12-12 19:57:15 -0800

    2023-12-13 #1582. Special Positions in a Binary Matrix

--- source/Generator1582.py
+++ source/Generator1582.py
@@ -1,3 +1,4 @@
+from helpers import matrix
 import random
 
 '''
@@ -5,18 +6,37 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_width = 100
+    max_height = 100
+    max_width_height = 100
+    minval = 0
+    maxval = 0
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for width in [1, 4, 10, max_width]:
+        height = min(max_height, max_width_height // width)
+        mymatrix = matrix.get_matrix(height, width, minval, maxval)
+        mymatrix[random.randint(0, height - 1)][random.randint(0, width - 1)] = 1
+        if width > 1 and height > 1:
+            mymatrix[random.randint(0, height - 1)][random.randint(0, width - 1)] = 1
+        tests.append(mymatrix.__str__().replace(' ', ''))
+
+    maxval = 1
+    for width in [2, 50, max_width]:
+        height = min(max_height, max_width_height // width)
+        mymatrix = matrix.get_matrix(height, width, minval, maxval)
+        tests.append(mymatrix.__str__().replace(' ', ''))
+
+    minval = 1
+    maxval = 1
+    height = 10
+    width = 10
+    mymatrix = matrix.get_matrix(height, width, minval, maxval)
+    for _ in range(10):
+        mymatrix[random.randint(0, height - 1)][random.randint(0, width - 1)] = 0
+    tests.append(mymatrix.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
+
+if __name__ == '__main__':
+    print(generate())
