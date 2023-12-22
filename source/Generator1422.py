c0f27e1fe35ecb85b37094a628bc868b4a14ce1e
Marius Stuebs <mqs@stuebs.tech>
2023-12-21 22:14:23 -0800

    2023-12-22 #1422. Maximum Score After Splitting a String

--- source/Generator1422.py
+++ source/Generator1422.py
@@ -6,17 +6,16 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 500
+    minval = 0
+    maxval = 1
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, min_num, max_num//2, max_num//2, max_num, max_num, max_num, max_num]:
+        test = [random.randint(minval, maxval).__str__() for _ in range(n)]
+        tests.append('"' + ''.join(test) + '"')
+
     return '''
 '''.join(tests)
+
+if __name__ == '__main__':
+    print(generate())
