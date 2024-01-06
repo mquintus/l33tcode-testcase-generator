bf29b8a75f1a2835c98cc3cd93ac20de6f02c522
Marius Stuebs <mqs@stuebs.tech>
2024-01-05 21:30:35 -0800

    2024-01-06 #1235. Maximum Profit in Job Scheduling

--- source/Generator1235.py
+++ source/Generator1235.py
@@ -5,18 +5,20 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 5*10**4
+    minval = 1
+    maxval = 10000
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 10, 20, 50, 100, 200, 500, max_num]:
+        starttime = [random.randint(minval, maxval) for _ in range(n)]
+        endtime = [start + random.randint(minval, maxval) for start in starttime]
+        profit = [random.randint(minval, maxval) for _ in range(n)]
+        tests.append(
+            starttime.__str__().replace(' ', '') + "\n" +
+            endtime.__str__().replace(' ', '') + "\n" +
+            profit.__str__().replace(' ', '')
+            )
+
     return '''
 '''.join(tests)
