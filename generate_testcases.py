780dbf2a210a00fff05d4f722e664d8713a7b080
Marius Stuebs <mqs@stuebs.tech>
2024-01-15 01:01:12 +0100

    'Testcase generator for challenge: #2225. Find Players With Zero or One Losses'

--- generate_testcases.py
+++ generate_testcases.py
@@ -237,6 +237,7 @@ def main(challenge_id=-1):
         1704,
         1347,
         1657,
+        2225,
         # insert next challenge here
     ]
     if int(challenge_id) == -1:
@@ -1222,6 +1223,10 @@ def main(challenge_id=-1):
     if int(challenge_id) == 1657:
         from source import Generator1657
         tests = Generator1657.generate()
+
+    if int(challenge_id) == 2225:
+        from source import Generator2225
+        tests = Generator2225.generate()
 
     # insert next challenge import here
 
