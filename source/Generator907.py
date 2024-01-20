77bc67300f020ee452fd682003a6450864c606c3
Marius Stuebs <mqs@stuebs.tech>
2024-01-19 22:08:00 -0800

    2024-01-20 #907. Sum of Subarray Minimums

--- source/Generator907.py
+++ source/Generator907.py
@@ -5,18 +5,25 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 3 * 10**4
+    minval = 1
+    maxval = 3 * 10**4
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 100, 1000]:
+        test = [random.randint(minval, maxval) for _ in range(n)]
+        tests.append(test.__str__().replace(' ', ''))
+
     n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
+    test = [random.randint(1, 9) for _ in range(n)]
     tests.append(test.__str__().replace(' ', ''))
-    
+
+    test = [random.randint(1, 99) for _ in range(n)]
+    tests.append(test.__str__().replace(' ', ''))
+
+    test = [random.randint(1, maxval) for _ in range(n)]
+    tests.append(test.__str__().replace(' ', ''))
+
+
     return '''
 '''.join(tests)
