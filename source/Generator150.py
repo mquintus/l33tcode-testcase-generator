a82babd831b6c9110c8a2f7f1346490cc69d4635
Marius Stuebs <mqs@stuebs.tech>
2024-01-29 22:37:32 -0800

    2024-01-30 #150. Evaluate Reverse Polish Notation

--- source/Generator150.py
+++ source/Generator150.py
@@ -6,17 +6,52 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**4 // 2 - 2
+    minval = -200
+    maxval = 200
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 5, 10, 20, 100, 200, 1000, max_num]:
+        value = random.randint(minval, maxval)
+        test = [str(value)]
+        i = 1
+        while i < n:
+            result = 2147483648
+            while result > 2147483647 or result < -2147483648:
+                nextval = random.randint(minval, maxval)
+                if value == 0 and nextval == 0:
+                    operator = random.choice(['+', '-', '*'])
+                else:
+                    operator = random.choice(['+', '-', '*', '/'])
+                nextpos = random.randint(-1, 0)
+                if operator == '/':
+                    nextpos = 0
+                    if value == 0:
+                        nextpos = -1
+
+                left,right = value, nextval
+                if nextpos == 0:
+                    left,right = nextval, value
+                if operator == '+':
+                    result = left + right
+                elif operator == '-':
+                    result = left - right
+                elif operator == '*':
+                    result = left * right
+                else:
+                    if (left < 0 and right > 0) or (left > 0 and right < 0):
+                        result = -((-left) // right)
+                    else:
+                        result = left // right
+            value = result
+            #print(left, operator, right, '=', result)
+            if nextpos == -1:
+                test.append(str(nextval))
+            else:
+                test.insert(nextpos, str(nextval))
+            test.append(operator)
+            #print(test)
+            i += 1
+        tests.append(test.__str__().replace(' ', '').replace("'", '"'))
+
     return '''
 '''.join(tests)
