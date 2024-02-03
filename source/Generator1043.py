9e6997320f850e09c99a9331dc29c2f34761ee2a
Marius Stuebs <mqs@stuebs.tech>
2024-02-02 19:41:55 -0800

    2024-02-03 generated/1043_Partition_Array_for_Maximum_Sum.txt source/Generator1043.py

--- source/Generator1043.py
+++ source/Generator1043.py
@@ -5,18 +5,29 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 500
+    minval = 0
+    maxval = 500
 
+    k = 1
     n = min_num
     test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    tests.append(test.__str__().replace(' ', '') + '\n' + str(k))
+
+    k = 2
+    n = 100
+    test = [0 if i % 4 in [1,2] else random.randint(minval, maxval) for i in range(n)]
+    tests.append(test.__str__().replace(' ', '') + '\n' + str(k))
+
+    for k in [1, 10, random.randint(400, 500)]:
+        n = max_num
+        test = [i for i in range(n // 2)] + [i for i in range(n // 2, 0, -1)]
+        tests.append(test.__str__().replace(' ', '') + '\n' + str(k))
+
+        n = max_num
+        test = [random.randint(minval, maxval) for _ in range(n)]
+        tests.append(test.__str__().replace(' ', '') + '\n' + str(k))
+
     return '''
 '''.join(tests)
