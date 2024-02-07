0e5680891332e5fecc1c7002c221d63336a88b8b
Marius Stuebs <mqs@stuebs.tech>
2024-02-06 16:46:07 -0800

    2024-02-07 #451. Sort Characters By Frequency

--- source/Generator451.py
+++ source/Generator451.py
@@ -5,18 +5,22 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 5 * 10**5
+    somechars = ""
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    return '''
-'''.join(tests)
+    # To make sure that non-anagram strings are tested
+    # We have to use different alphabets for the two strings
+    for char in range(ord('a'), ord('z')+1):
+        somechars += chr(char)
+    for char in range(ord('A'), ord('Z')+1):
+        somechars += chr(char)
+    for char in range(ord('1'), ord('9')+1):
+        somechars += chr(char)
+    somechars += '0'
+
+    for n in [min_num, 10, 20, 50, 100, 1000, max_num]:
+        string1 = "".join([random.choice(somechars) for _ in range(n)])
+        tests.append(f'"{string1}"')
+
+    return "\n".join(tests)
