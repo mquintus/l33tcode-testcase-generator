c3e8df5febe240aefc4b3a358fbac2a05c865622
Marius Stuebs <mqs@stuebs.tech>
2024-02-11 19:33:01 -0800

    2024-02-12 #169. Majority Element

--- source/Generator169.py
+++ source/Generator169.py
@@ -5,18 +5,28 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 5 * 10**4
+    minval = -10**9
+    maxval = 10**9
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 20, 20, 21, 100, 100, 101, 5000]:
+        # Edgecase 6 - The frequency of each integer is different except for 1 frequency which is there twice
+        majority_element_frequency = n//2 + 1
+        freq_sum = majority_element_frequency
+        freqs = {42: majority_element_frequency}
+        while freq_sum < n:
+            freq = random.randint(1, n - freq_sum)
+            while freq_sum + freq > n:
+                freq -= 1
+            freq_sum += freq
+            el = random.randint(minval, maxval)
+            freqs[el] = freq
+        matrix = [[el] * freq for el, freq in freqs.items()]
+        test = [item for row in matrix for item in row]
+        #test.sort()
+        random.shuffle(test)
+        tests.append(f'{test}'.replace(" ", ""))
+
     return '''
 '''.join(tests)
