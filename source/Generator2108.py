852d3e43fb0d997a5cb4d1afc8bfdef1e2c1b6c2
Marius Stuebs <mqs@stuebs.tech>
2024-02-12 16:35:54 -0800

    2024-02-13 #2108. Find First Palindromic String in the Array

--- source/Generator2108.py
+++ source/Generator2108.py
@@ -5,18 +5,52 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 100
+    minval = 1
+    maxval = 100
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    count = 1
+    test = []
+    for _ in range(count):
+        length = 1
+        test.append('"' + "".join([random.choice(alphabet) for _ in range(length)]) + '"')
+    tests.append(test.__str__().replace(' ', '').replace("'", ''))
+
+    for count in [2, 3, max_num]:
+        test = []
+        for _ in range(count):
+            length = random.randint(minval, maxval)
+            test.append('"' + "".join([random.choice(alphabet) for _ in range(length)]) + '"')
+        tests.append(test.__str__().replace(' ', '').replace("'", ''))
+
+        test = []
+        for _ in range(count):
+            length = random.randint(minval, maxval)
+            word = [random.choice(alphabet) for _ in range(length)]
+            if random.randint(0, 1) == 0:
+                word = word[:length//2]
+                word = word + word[::-1]
+            test.append('"' + "".join(word) + '"')
+        tests.append(test.__str__().replace(' ', '').replace("'", ''))
+
+    count = max_num - 1
+    test = []
+    for _ in range(count):
+        length = random.randint(maxval - 10, maxval - 2)
+        word = [random.choice(alphabet) for _ in range(length)]
+        word = word[:length//2]
+        word = word + ["xy"] + word[::-1]
+        test.append('"' + "".join(word) + '"')
+    length = random.randint(maxval - 10, maxval - 2)
+    word = [random.choice(alphabet) for _ in range(length)]
+    word = word[:length//2]
+    word = word + word[::-1]
+    test.append('"' + "".join(word) + '"')
+    tests.append(test.__str__().replace(' ', '').replace("'", ''))
+
+
     return '''
 '''.join(tests)
