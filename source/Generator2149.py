6d01501f0e93651688f72cbefc64fb80c2b2cc89
Marius Stuebs <mqs@stuebs.tech>
2024-02-13 16:31:56 -0800

    2024-02-14 #2149. Rearrange Array Elements by Sign

--- source/Generator2149.py
+++ source/Generator2149.py
@@ -6,17 +6,15 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**5
+    minval = 1
+    maxval = 10**4
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 4, 6, 40, 500, 6000, 70000, max_num]:
+        test = [-1 * random.randint(minval, maxval) for _ in range(n // 2)]
+        test.extend([random.randint(minval, maxval) for _ in range(n // 2)])
+        random.shuffle(test)
+        tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
