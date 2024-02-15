1ad11b1f3e4b270b8a37c2e6edaf0c96ca7ffdb5
Marius Stuebs <mqs@stuebs.tech>
2024-02-14 19:43:54 -0800

    2024-02-15  #2971. Find Polygon With the Largest Perimeter

--- source/Generator2971.py
+++ source/Generator2971.py
@@ -5,18 +5,24 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 3
+    max_num = 10**5
+    minval = 1
+    maxval = 10**9
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, 40, 500, 10000]:
+        test = [random.randint(minval, maxval) for _ in range(n)]
+        tests.append(test.__str__().replace(' ', ''))
+
+        test = []
+        running = 0
+        for _ in range(n):
+            test.append(random.randint(minval + running, minval + running + 1))
+            running += test[-1]
+            if running > maxval:
+                break
+        print(len(test))
+        tests.append(test.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
