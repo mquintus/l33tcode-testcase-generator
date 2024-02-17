1d847d0c594633434c1ecd46fd848d74e14e8775
Marius Stuebs <mqs@stuebs.tech>
2024-02-16 17:41:39 -0800

    2024-02-17 1642_Furthest_Building_You_Can_Reach.txt

--- source/Generator1642.py
+++ source/Generator1642.py
@@ -5,18 +5,73 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+    minval = 1
+    maxval = 10**6
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+
+    second = False
+    for n in [20, 10**3]:
+        first = True
+        # Testcase 1 - The distance between buildings is increasing
+        distances = sorted([random.randint(minval, 10) for _ in range(n)])
+        heights = [10000 * random.randint(1, 9)]
+        up = -1
+        for d in distances:
+            heights.append(min(maxval, max(1, heights[-1] + d*up)))
+            if not second:
+                up *= -1
+        bricks = random.randint(min_num, max_num)
+        ladders = random.randint(min_num, len(heights)-1) // 2
+        if first:
+            bricks = ladders = 0
+            first = False
+            second = True
+        tests.append(f'{heights}\n{bricks}\n{ladders}'.replace(' ', ''))
+
+        # Testcase 2 - The distance between buildings is decreasing
+        distances = sorted([random.randint(minval, maxval) for _ in range(n )])[::-1]
+        heights = [10000 * random.randint(1, 9)]
+        up = 1
+        for d in distances:
+            heights.append(min(maxval, max(1, heights[-1] + d*up)))
+            up *= -1
+        bricks = random.randint(min_num, max_num)
+        ladders = random.randint(min_num, len(heights)-1)
+        tests.append(f'{heights}\n{bricks}\n{ladders}'.replace(' ', ''))
+
+        # Testcase 3 - The distance between buildings is random
+        # but the bricks sum up to the actual distance - the max distance
+        # and we have only one ladder
+        distances = [random.randint(minval, maxval) for _ in range(n )]
+        heights = [10000 * random.randint(1, 9)]
+        up = -1
+        actual_distances = 0
+        max_distance = 0
+        for d in distances:
+            heights.append(min(maxval, max(1, heights[-1] + d*up)))
+            if up == 1:
+                actual_distances += d
+                max_distance = max(max_distance, d)
+            up *= -1
+        bricks = actual_distances - max_distance
+        ladders = 1
+        tests.append(f'{heights}\n{bricks}\n{ladders}'.replace(' ', ''))
+
+        # Testcase 4 - Up up down down left right left right B A Start
+        up = 1
+        distances = sorted([random.randint(minval, maxval) for _ in range(n //5)])[::-1]
+        distances.extend(sorted([random.randint(minval, maxval) for _ in range(n //5)]))
+        distances.extend(sorted([random.randint(minval, maxval) for _ in range(n //5)][::-1]))
+        distances.extend(sorted([random.randint(minval, maxval) for _ in range(n //5)]))
+        heights = [10000 * random.randint(1, 9)]
+        for d in distances:
+            heights.append(min(maxval, max(1, heights[-1] + d*up)))
+            up *= -1
+        bricks = random.randint(min_num, max_num)
+        ladders = random.randint(min_num, len(heights)-1)
+        tests.append(f'{heights}\n{bricks}\n{ladders}'.replace(' ', ''))
+
     return '''
 '''.join(tests)
