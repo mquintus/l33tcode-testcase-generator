2032017a422e1af5d0d707bea8506d50f22118d8
Marius Stuebs <mqs@stuebs.tech>
2024-02-20 21:15:02 -0800

    2024-02-21 #201. Bitwise AND of Numbers Range

--- source/Generator201.py
+++ source/Generator201.py
@@ -5,18 +5,20 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 0
+    max_num = 2*31 - 1
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for _ in range(4):
+        left = random.randint(min_num, max_num - 1)
+        right = random.randint(left, max_num)
+        tests.append(left.__str__() + '\n' + right.__str__())
+
+    for _ in range(4):
+        left_exp = random.randint(20, 30)
+        right_exp = random.randint(left_exp + 1, 31)
+        left = 2 ** left_exp
+        right = (2 ** right_exp) - random.randint(1, 31)
+        tests.append(left.__str__() + '\n' + right.__str__())
+
     return '''
 '''.join(tests)
