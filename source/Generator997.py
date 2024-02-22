46896f77b3bbba227d5a5c0e1085cdfb3f450eb7
Marius Stuebs <mqs@stuebs.tech>
2024-02-21 18:23:40 -0800

    2024-02-22 997_Find_the_Town_Judge.txt

--- source/Generator997.py
+++ source/Generator997.py
@@ -5,18 +5,39 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 1000
+    max_relations = 9999
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [1, 2, 3, 4, 5, 6, max_num - 1, max_num]:
+        judge = random.randint(1, n)
+        if n % 2 == 1:
+            judge = 0
+        trust = []
+        relations = 0
+        abort = False
+        #print(judge)
+        if judge != 0:
+            for i in range(1,n+1):
+                if i == judge:
+                    continue
+                relations += 1
+                trust.append([i,judge])
+
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if i != j and (j != judge and (i != judge and random.randint(0,1) == 1)):
+                    relations += 1
+                    trust.append([i,j])
+                    if relations == max_relations:
+                        abort = True
+                        break
+                if abort:
+                    break
+            if abort:
+                break
+
+        tests.append(n.__str__() + "\n" + trust.__str__().replace(' ', ''))
+
     return '''
 '''.join(tests)
