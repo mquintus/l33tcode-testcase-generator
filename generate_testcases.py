5e9b741fbd29d96ccc4b32e32e9dbf8433001fab
Marius Stuebs <mqs@stuebs.tech>
2024-02-29 01:01:10 +0100

    'Testcase generator for challenge: #1609. Even Odd Tree'

--- generate_testcases.py
+++ generate_testcases.py
@@ -283,6 +283,7 @@ def main(challenge_id=-1):
         100,
         543,
         513,
+        1609,
         # insert next challenge here
     ]
     if int(challenge_id) == -1:
@@ -1452,6 +1453,10 @@ def main(challenge_id=-1):
     if int(challenge_id) == 513:
         from source import Generator513
         tests = Generator513.generate()
+
+    if int(challenge_id) == 1609:
+        from source import Generator1609
+        tests = Generator1609.generate()
 
     # insert next challenge import here
 
