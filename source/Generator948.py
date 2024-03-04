14f1b4f71b7c5e17842143609f8dba4311a493a6
Marius Stuebs <mqs@stuebs.tech>
2024-03-03 17:40:28 -0800

    2024-03-04 948_Bag_of_Tokens.txt

--- source/Generator948.py
+++ source/Generator948.py
@@ -5,18 +5,40 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 0
+    max_num = 1000
+    minval = 0
+    maxval = 100
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    # Edgecases:
+    # 1. Zero elements, lots of power
+    # 2. Zero elements, no power
+    # 3. One element, lots of power
+    # 4. One element, no power
+    # 5. Two elements, lots of power
+    # 6. Two elements, no power
+
+    for n in [min_num, 1, 2, 3]:
+        for power in range(0, n+1):
+            tokens = [max(minval, power - 1 + i) for i in range(n)]
+            test = tokens.__str__().replace(' ', '')
+            test += f"\n{power}"
+            tests.append(test)
+
+    offset = random.randint(minval + maxval // 2, maxval)
     n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for power in [1, n]:
+        tokens = [offset + i for i in range(n)]
+        test = tokens.__str__().replace(' ', '')
+        test += f"\n{power}"
+        tests.append(test)
+
+    n = max_num
+    for power in [1, n]:
+        tokens = [random.randint(1,99) for i in range(n)]
+        test = tokens.__str__().replace(' ', '')
+        test += f"\n{power}"
+        tests.append(test)
+
     return '''
 '''.join(tests)
