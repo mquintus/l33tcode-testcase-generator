63a93e85cf9aeb71feb093807c6d62f096b83202
Marius Stuebs <mqs@stuebs.tech>
2024-03-26 09:59:26 -0700

    2024-03-26 #41. First Missing Positive

--- source/Generator41.py
+++ source/Generator41.py
@@ -5,18 +5,47 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+    minval = -10**5
+    maxval = 10**5
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
+    # Testcase 1: All numbers are present
+    n = 20
+    test = [n - i for i in range(n)]
     tests.append(test.__str__().replace(' ', ''))
-    
+
+    # Testcase 2 - 4: First, second, last, number is missing
+    n = 20
+    for missing in [0, 1, n-1]:
+        test = [n - i for i in range(n)]
+        if random.choice([True, False]):
+            test[missing] = random.randint(n+1, maxval)
+        else:
+            test[missing] = random.randint(minval, 0)
+        tests.append(test.__str__().replace(' ', ''))
+
+    # Testcase 5: All numbers are double
+    n = 20
+    test = [int(i//2) for i in range(n)]
+    tests.append(test.__str__().replace(' ', ''))
+
+    # Testcase 6: All numbers are tripled
+    n = 30
+    test = [int(i//3) for i in range(n)]
+    tests.append(test.__str__().replace(' ', ''))
+
+    # Testcase 7: Minimal testcase
+    test = [min_num]
+    tests.append(test.__str__().replace(' ', ''))
+
+    # Testcase 8: Maximal testcase
     n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
+    test = [int(i//2) for i in range(2,n)]
+    random.shuffle(test)
     tests.append(test.__str__().replace(' ', ''))
-    
+
+
+
     return '''
 '''.join(tests)
