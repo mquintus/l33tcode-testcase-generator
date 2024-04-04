ce3c751a45a3c688f73384d4eabf344e15b2e397
Marius Stuebs <mqs@stuebs.tech>
2024-04-03 17:56:36 -0700

    2024-04-04 #1614. Maximum Nesting Depth of the Parentheses

--- source/Generator1614.py
+++ source/Generator1614.py
@@ -6,17 +6,37 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 100
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+
+    alphabet ='0123456789+-/'
+
+    test = '"()"'
+    tests.append(test)
+
+    test = '"()()"'
+    tests.append(test)
+
+    test = '"42"'
+    tests.append(test)
+
+    for n in [20, 20, max_num, max_num]:
+        p = random.randint(1, n)
+        test = [random.choice(alphabet) for _ in range(n)]
+        for i in range(p):
+            pos = random.randint(0, len(test)-2)
+            if test[pos] in ['(', ')']:
+                continue
+            test[pos] = '('
+            start = random.randint(pos, len(test)-1)
+            for pos2 in range(start, len(test)):
+                if test[pos2] in ['(', ')']:
+                    continue
+                test[pos2] = ')'
+                break
+            else:
+                test[pos] = '*'
+        tests.append('"' + "".join(test) + '"')
+
     return '''
 '''.join(tests)
