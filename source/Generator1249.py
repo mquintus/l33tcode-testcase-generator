f9c8d392da9229e4d779a7169a402a3fe9181135
Marius Stuebs <mqs@stuebs.tech>
2024-04-05 21:15:01 -0700

    2024-04-06 #1249. Minimum Remove to Make Valid Parentheses

--- source/Generator1249.py
+++ source/Generator1249.py
@@ -5,18 +5,22 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+#    max_num = 10**3 * 2
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    for n in [min_num, min_num+1, max_num-1, max_num]:
+        for a in range(2):
+            test = []
+            for i in range(n):
+                rand = random.randint(0, 10)
+                if rand == 0:
+                    test.append(random.choice('abcdefghijklmnopqrstuvwxyz'))
+                elif rand < 5:
+                    test.append('(')
+                else:
+                    test.append(')')
+            tests.append('"' + ''.join(test) + '"')
+
     return '''
 '''.join(tests)
