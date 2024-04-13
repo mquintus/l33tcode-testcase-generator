f3e362c18b38a8d0e98bba242aa4237c054bd997
Marius Stuebs <mqs@stuebs.tech>
2024-04-12 17:35:38 -0700

    2024-04-13 #85. Maximal Rectangle

--- source/Generator85.py
+++ source/Generator85.py
@@ -5,18 +5,25 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 200
+    #max_num = 35
+    minval = 0
+    maxval = 1
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    line = 0
+    for num_rows in [min_num+1, max_num-42, max_num]:
+        for num_cols in [min_num+1, max_num // 2, max_num]:
+            line += 1
+            if line == 2 or line == 3:
+                continue
+            print("line", line )
+            matrix = []
+            for row in range(num_rows):
+                matrix.append([])
+                for col in range(num_cols):
+                    matrix[-1].append('"' + str(min(maxval, random.randint(minval, 4)))+'"')
+            tests.append(matrix.__str__().replace(' ', '').replace("'", ""))
+
     return '''
 '''.join(tests)
