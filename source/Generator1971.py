ac5ba3f10fe7d07716b3e7b04b3b09ece372c12a
Marius Stuebs <mqs@stuebs.tech>
2024-04-20 18:48:25 -0700

    #1971. Find if Path Exists in Graph

--- source/Generator1971.py
+++ source/Generator1971.py
@@ -5,18 +5,35 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 200
+    min_num = 1
+    edgecount_max = 200 # 2*10**5
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [min_num, 2, 20, 45, 90, max_num]:
+      graph = []
+      edgecount = 0
+      possible_edges = n*(n+1)
+      allowed_edge_fraction = min(0.7,  edgecount_max / possible_edges) * 1000000
+      for f in range(0, n):
+        for t in range(f+1, n):
+          if random.randint(1, 1000000) < allowed_edge_fraction:
+             edge = [f,t]
+             random.shuffle(edge)
+             graph.append(edge)
+      test = f"{n}\n{graph}\n0\n{n-1}".replace(' ', '')
+      tests.append(test)
     
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    graph = []
+    for f in range(0, n-1):
+        graph.append([f, f+1])
+    test = f"{n}\n{graph}\n0\n{n-1}".replace(' ', '')
+    tests.append(test)
+    
+    graph = []
+    for f in range(0, n-10):
+        graph.append([f, f+1])
+    test = f"{n}\n{graph}\n{n-5}\n{n-5}".replace(' ', '')
+    tests.append(test)
     
     return '''
 '''.join(tests)
