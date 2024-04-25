1dcb6815f575a7694c6660d3cc141baf88dd2f23
Marius Stuebs <mqs@stuebs.tech>
2024-04-24 22:18:32 -0700

    2024-04-24 #2370. Longest Ideal Subsequence

--- source/Generator2370.py
+++ source/Generator2370.py
@@ -5,18 +5,20 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+    min_k = 0
+    max_k = 25
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [min_num, 100, 1000, 10000, max_num, max_num, max_num]:
+      test = "".join([random.choice(alphabet) for _ in range(n)])
+      k = random.randint(min_k, max_k)
+      tests.append('"' + test + '"\n' + str(k))
+
+    test = "a"*max_num
+    k = 25
+    tests.append('"' + test + '"\n' + str(k))
     
     return '''
 '''.join(tests)
