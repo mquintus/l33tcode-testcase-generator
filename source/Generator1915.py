66e38ce61fd85ebc3651c56f466bdcd0de61d6b5
Marius Stuebs <mqs@stuebs.tech>
2024-04-30 10:49:56 -0700

    2024-04-30 #1915. Number of Wonderful Substrings

--- source/Generator1915.py
+++ source/Generator1915.py
@@ -5,18 +5,14 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+#   max_num = 7 * 10**3
+    max_num = 10**5
+    alphabet = 'abcdefhgji'
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [min_num, min_num+1,min_num+2,min_num+3,100,1000,max_num//10,max_num]:
+      test = '"' + "".join([random.choice(alphabet) for _ in range(n)]) + '"'
+      tests.append(test.__str__().replace(' ', ''))
     
     return '''
 '''.join(tests)
