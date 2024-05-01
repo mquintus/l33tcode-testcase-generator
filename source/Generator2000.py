d96a5075fdff8ab8d14a28c55d48875f1b46a9dc
Marius Stuebs <mqs@stuebs.tech>
2024-05-01 09:51:50 -0700

    2024-05-01 #2000. Reverse Prefix of Word

--- source/Generator2000.py
+++ source/Generator2000.py
@@ -5,18 +5,32 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 250
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    subalpha = alphabet[1:]
 
+    k = 'a'
     n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    test = [random.choice(subalpha) for _ in range(n)]
+    tests.append('"'+"".join(test)+'"\n"'+k+'"')
     
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [20, max_num - 2]:
+        test = [random.choice(subalpha) for _ in range(n)]
+        tests.append('"'+"".join(test)+'"\n"'+k+'"')
+        
+        test = [random.choice(alphabet) for _ in range(n)]
+        test.insert(0,'a')
+        tests.append('"'+"".join(test)+'"\n"'+k+'"')
+
+        test = [random.choice(alphabet) for _ in range(n)]
+        test.append('a')
+        tests.append('"'+"".join(test)+'"\n"'+k+'"')
+
+    n = max_num - 2
+    test = [random.choice(subalpha) for _ in range(n)]
+    test.append('a')
+    tests.append('"'+"".join(test)+'"\n"'+k+'"')
     
     return '''
 '''.join(tests)
