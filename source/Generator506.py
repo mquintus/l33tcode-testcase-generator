7676883bd7c82d5af1369a2f9e62c5b045df0c0d
Marius Stuebs <marius@stuebs.tech>
2024-05-07 22:45:57 -0700

    2024-05-08 #506. Relative Ranks

--- source/Generator506.py
+++ source/Generator506.py
@@ -5,18 +5,21 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
+    min_num = 1
     max_num = 10**4
-    minval = -1000
-    maxval = 1000
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [min_num,
+              min_num+1,
+              min_num+2,
+              min_num+3,
+              11,
+              10**2,
+              10**3,
+              max_num]:
+        mylist=[i*3 for i in range(n)]
+        random.shuffle(mylist)
+        tests.append(str(mylist).replace(" ", ""))
+
     
     return '''
 '''.join(tests)
