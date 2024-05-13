129dd738b496f6f11e241e35b3fb306bc5fb80c2
Marius Stuebs <marius@stuebs.tech>
2024-05-12 19:49:25 -0700

    2024-05-13 #861. Score After Flipping Matrix

--- source/Generator861.py
+++ source/Generator861.py
@@ -5,18 +5,15 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 20
+    minval = 0
+    maxval = 1
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [min_num, 10, max_num]:
+        for m in [min_num, 2, 5, max_num]:
+            grid = [[random.randint(minval, maxval) for _ in range(n)] for _ in range(m)]
+            tests.append(grid.__str__().replace(' ', ''))
     
     return '''
 '''.join(tests)
