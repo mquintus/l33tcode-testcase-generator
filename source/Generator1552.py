959e3211049898a17fabcccb090bcf353358793a
Marius Stuebs <mqs@stuebs.tech>
2024-06-20 12:40:33 +0200

    2024-06-20 Testcases for 1552 Magnetic Force between two balls

--- source/Generator1552.py
+++ source/Generator1552.py
@@ -6,17 +6,22 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**5
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
     n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    test = [int(((1 + i)*10) ** 1.1) for i in range(n)]
+    #random.shuffle(test)
+    m = max_num // 7
+    tests.append(test.__str__().replace(' ', '') + "\n" + str(m))
+
+    for n in [min_num, 1000, max_num]:
+        for m in [2,3,max_num//7]:
+            if m > n:
+                continue
+            test = [20 * (i+1) + random.randint(-9, 9) for i in range(n)]
+            random.shuffle(test)
+            tests.append(test.__str__().replace(' ', '') + "\n" + str(m))
+            #max_val *= 10
     
     return '''
 '''.join(tests)
