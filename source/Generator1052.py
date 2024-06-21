80c97dc88106f9ed641518dfdea499a8dec62342
Marius Stuebs <mqs@stuebs.tech>
2024-06-21 11:25:02 +0200

    2024-06-21 Grumpy Bookstore Owner

--- source/Generator1052.py
+++ source/Generator1052.py
@@ -5,18 +5,52 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 2*10**4
+    minval = 0
+    maxval = 999
+    
+    # Testcase 1 - All days are grumpy
+    n = 50
+    customers = [random.randint(minval, maxval) for _ in range(n)]
+    grumpy = [1 for _ in range(n)]
+    minutes = random.randint(1, n)
+    tests.append(f'{customers}\n{grumpy}\n{minutes}'.replace(" ", ""))
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    # Testcase 2 - No days are grumpy
+    n = 50
+    customers = [random.randint(minval, maxval) for _ in range(n)]
+    grumpy = [0 for _ in range(n)]
+    minutes = random.randint(1, n)
+    tests.append(f'{customers}\n{grumpy}\n{minutes}'.replace(" ", ""))
+
+    # Testcase 3 - All days are grumpy but no minutes
+    n = 50
+    customers = [random.randint(minval, maxval) for _ in range(n)]
+    grumpy = [1 for _ in range(n)]
+    minutes = 1
+    tests.append(f'{customers}\n{grumpy}\n{minutes}'.replace(" ", ""))
+
+    ## Testcase 4 - Random values, max length
     n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    customers = [random.randint(minval, maxval) for _ in range(n)]
+    grumpy = [random.choice([0,1]) for _ in range(n)]
+    minutes = random.randint(n // 3, (2 * n) // 3)
+    tests.append(f'{customers}\n{grumpy}\n{minutes}'.replace(" ", ""))
+
+    # Testcase 5 - Increasing customers, alternating grumpiness
+    n = max_num
+    customers = [int(i*1000/n) for i in range(1, n+1)]
+    grumpy = [random.choice([0,1]) for _ in range(n)]
+    minutes = random.randint(n // 7, (2 * n) // 7)
+    tests.append(f'{customers}\n{grumpy}\n{minutes}'.replace(" ", ""))
+
+    # Testcase 6 - Modulo customers, reverse, alternating grumpiness
+    n = max_num
+    customers = [1+((-i)%42)+random.randint(0,5) for i in range(1, n+1)]
+    grumpy = [random.choice([0,1]) for _ in range(n)]
+    minutes = random.randint(n // 7, (2 * n) // 7)
+    tests.append(f'{customers}\n{grumpy}\n{minutes}'.replace(" ", ""))
+
     return '''
 '''.join(tests)
