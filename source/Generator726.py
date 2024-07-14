6bd0533c27ccf9c439a43ac1c0d4ffa57e3c45e1
mquintus <mqs@stuebs.tech>
2024-07-14 13:51:33 +0200

    Update Generator726.py

--- source/Generator726.py
+++ source/Generator726.py
@@ -4,19 +4,10 @@ import random
 726 - Number of Atoms
 '''
 def generate() -> str:
-    tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
-
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    return '''
-'''.join(tests)
+    return '''"(OH)2(OH)2"
+"((CH3)2(C2H5)3)2(Mg(CO3)2)"
+"Ca(OH)2Mg(OH)2"
+"K4(ON(SO3)2)3(Fe(CN)6)2"
+"(((K4(ON(SO3)2)3(Fe(CN)6)2)4(Mg(OH)2)10)2(H2O)5)3"
+"(((K4(ON(SO3)2)3(Fe(CN)6)2)10(Mg(OH)2)20)5(H2O)50)6"
+'''
