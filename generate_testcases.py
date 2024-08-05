f18b23cb1e13024a736091d3295caaad418e4426
Marius Stuebs <mqs@stuebs.tech>
2024-08-05 02:01:12 +0200

    'Testcase generator for challenge: #2053. Kth Distinct String in an Array'

--- generate_testcases.py
+++ generate_testcases.py
@@ -440,6 +440,7 @@ def main(challenge_id=-1):
         2134,
         1460,
         1508,
+        2053,
         # insert next challenge here
     ]
     if int(challenge_id) == -1:
@@ -2237,6 +2238,10 @@ def main(challenge_id=-1):
     if int(challenge_id) == 1508:
         from source import Generator1508
         tests = Generator1508.generate()
+
+    if int(challenge_id) == 2053:
+        from source import Generator2053
+        tests = Generator2053.generate()
 
     # insert next challenge import here
 
