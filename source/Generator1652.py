ed1abf4969cfb1bbdd68777d229bd9d1c8c2ea9c
Marius Stuebs <mqs@stuebs.tech>
2024-11-18 09:50:59 +0100

    Add generator and exanole for 1652 defuse the bomb

--- source/Generator1652.py
+++ source/Generator1652.py
@@ -5,18 +5,25 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 100
+    minval = 1
+    maxval = 100
 
-    n = min_num
+    n = random.randint(5, 10)
+    code = 0
     test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    tests.append(test.__str__().replace(' ', '') + "\n" + code.__str__())
+
+    for n in [min_num, 2, 23, 42, 73, 99]:
+        code = random.randint(-n+1, n-1)
+        test = [random.randint(minval, maxval) for _ in range(n)]
+        tests.append(test.__str__().replace(' ', '') + "\n" + code.__str__())
+
     n = max_num
+    code = max_num-1
     test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
+    tests.append(test.__str__().replace(' ', '') + "\n" + code.__str__())
+
     return '''
 '''.join(tests)
