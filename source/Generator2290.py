42c0298f0b17436435ac43d816a4e9f6e4f04a18
Marius Stuebs <mqs@stuebs.tech>
2024-11-28 09:19:48 +0100

    Add TLE testcases and flag to generate non-TLE testcases for Challenge 2290

--- source/Generator2290.py
+++ source/Generator2290.py
@@ -5,18 +5,47 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    tle = True
+    #tle = False
+    maxrows = 10**5
+    maxcols = 10**5
+    minmult = 2
+    maxmult = 10**5
+    if not tle:
+        # Small testcases
+        maxrows = 250
+        maxcols = 250
+        minmult = 2
+        maxmult = 250
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for rows in [2, 5, 50, int((maxmult)**.5), maxrows // 50, maxmult // 2]:
+        test = []
+        cols = max(minmult, maxmult // rows)
+        for r in range(rows):
+            test.append([])
+            for c in range(cols):
+                test[-1].append(random.choice([0, 1, 1]))
+        test[0][0] = 0
+        test[rows - 1][cols - 1] = 0
+        tests.append(test.__str__().replace(' ',''))
     
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    # Simple maze
+    rows = int((maxmult)**.5) // 2
+    cols = maxcols // 5
+    test = [[1] * cols for _ in range(rows)]
+    for r in range(1, rows//2):
+        c = max(0, cols - 1 - random.randint(0, 10))
+        test[r][c] = 0
+    for r in range(rows//2, rows):
+        c = min(cols-1,  0 + random.randint(0, 10))
+        test[r][c] = 0
+    for r in [rows // 5, rows // 5 * 2, rows // 5 * 3, rows // 5 * 4]:
+        r = min(rows-1, r + random.randint(0, 10))
+        for c in range(1, cols):
+            test[r][c] = 0
+    test[0][0] = 0
+    test[rows - 1][cols - 1] = 0
+    tests.append(test.__str__().replace(' ',''))
     
     return '''
 '''.join(tests)
