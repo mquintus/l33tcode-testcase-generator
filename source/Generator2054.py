a1f69418f8f924ccf9c3c8e26641fcbb8c01051d
Marius Stuebs <mqs@stuebs.tech>
2024-12-08 11:05:33 +0100

    TLE testcases for 2054_Two_Best_Non-Overlapping_Events.txt

--- source/Generator2054.py
+++ source/Generator2054.py
@@ -6,17 +6,23 @@ import random
 def generate() -> str:
     tests = []
     min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    max_num = 10**5
+    minval = 1
+    maxval = 10**4
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [min_num, 
+              (min_num+max_num)//6, 
+              (min_num+max_num)//6*2, 
+              (min_num+max_num)//6*3, 
+              (min_num+max_num)//6*4, 
+              (min_num+max_num)//6*5, 
+              max_num]:
+        start = [random.randint(minval, maxval) for _ in range(n)]
+        duration = [random.randint(minval, maxval//100) for _ in range(n)]
+        end = [min(maxval, start[i] + duration[i]) for i in range(n)]
+        value = [random.randint(minval, maxval) for _ in range(n)]
+        test = [[s,e,v] for s,e,v in zip(start, end, value)]
+        tests.append(test.__str__().replace(' ', ''))
     
     return '''
 '''.join(tests)
