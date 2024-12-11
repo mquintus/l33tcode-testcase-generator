166c1ea271e631dfce3bd82111b07d46729bff27
Marius Stuebs <mqs@stuebs.tech>
2024-12-11 10:59:36 +0100

    Generator2779.py 2779_Maximum_Beauty_of_an_Array_After_Applying_Operation.txt

--- source/Generator2779.py
+++ source/Generator2779.py
@@ -5,18 +5,27 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+    minval = 0
+    maxval = 10**5
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    for n in [min_num,
+                (min_num+max_num)//6,
+                (min_num+max_num)//6*2,
+                (min_num+max_num)//6*3,
+                (min_num+max_num)//6*4,
+                (min_num+max_num)//6*5,
+                max_num
+              ]:
+        test = [random.randint(minval, maxval) for _ in range(n)]
+        k = random.randint(1, maxval)
+        tests.append(test.__str__().replace(' ', '')+"\n"+str(k))
     
     n = max_num
+    k = 0
     test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+    tests.append(test.__str__().replace(' ', '')+"\n"+str(k))
     
     return '''
 '''.join(tests)
