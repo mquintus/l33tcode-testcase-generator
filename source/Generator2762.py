90591820a85d4e36bf903d2a6cf7701eca954d7f
Marius Stuebs <mqs@stuebs.tech>
2024-12-14 12:17:33 +0100

    2762_Continuous_Subarrays.txt

--- source/Generator2762.py
+++ source/Generator2762.py
@@ -5,18 +5,26 @@ import random
 '''
 def generate() -> str:
     tests = []
-    min_num = 2
-    max_num = 10**4
-    minval = -1000
-    maxval = 1000
+    min_num = 1
+    max_num = 10**5
+    minval = 1
+    maxval = 10**9
 
-    n = min_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
+    n = 1
+    test = [maxval]
     tests.append(test.__str__().replace(' ', ''))
-    
-    n = max_num
-    test = [random.randint(minval, maxval) for _ in range(n)]
-    tests.append(test.__str__().replace(' ', ''))
+
+    for n in [min_num,
+              max_num//10,
+              max_num//5,
+              max_num//2,
+              max_num//2 + 1,
+              max_num - 2,
+              max_num - 1]:
+        test = [random.randint(minval, 999)]
+        for _ in range(n):
+            test.append(max(minval, test[-1]+random.randint(-3, 3)))
+        tests.append(test.__str__().replace(' ', ''))
     
     return '''
 '''.join(tests)
