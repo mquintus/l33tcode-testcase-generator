fa85b6630f8f0074fe9387e489c489eec1b7446b
Marius Stuebs <mqs@stuebs.tech>
2025-07-14 02:01:15 +0200

    'Testcase generator for challenge: #1290. Convert Binary Number in a Linked List to Integer'

--- generate_testcases.py
+++ generate_testcases.py
@@ -782,6 +782,7 @@ def main(challenge_id=-1):
         2402,
         1900,
         2410,
+        1290,
         # insert next challenge here
     ]
     if int(challenge_id) == -1:
@@ -3947,6 +3948,10 @@ def main(challenge_id=-1):
     if int(challenge_id) == 2410:
         from source import Generator2410
         tests = Generator2410.generate()
+
+    if int(challenge_id) == 1290:
+        from source import Generator1290
+        tests = Generator1290.generate()
 
     # insert next challenge import here
 
