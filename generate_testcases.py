746b4b8e8c33c0930dc4dfd06d151e1582badf6a
Marius Stuebs <mqs@stuebs.tech>
2025-09-10 02:01:14 +0200

    'Testcase generator for challenge: #1733. Minimum Number of People to Teach'

--- generate_testcases.py
+++ generate_testcases.py
@@ -840,6 +840,7 @@ def main(challenge_id=-1):
         1304,
         1317,
         2327,
+        1733,
         # insert next challenge here
     ]
     if int(challenge_id) == -1:
@@ -4237,6 +4238,10 @@ def main(challenge_id=-1):
     if int(challenge_id) == 2327:
         from source import Generator2327
         tests = Generator2327.generate()
+
+    if int(challenge_id) == 1733:
+        from source import Generator1733
+        tests = Generator1733.generate()
 
     # insert next challenge import here
 
