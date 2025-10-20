9d4d11c3a9bf2d26da8cb93713ae70e5418f68a5
Marius Stuebs <mqs@stuebs.tech>
2025-10-20 02:01:14 +0200

    'Testcase generator for challenge: #2011. Final Value of Variable After Performing Operations'

--- generate_testcases.py
+++ generate_testcases.py
@@ -880,6 +880,7 @@ def main(challenge_id=-1):
         3003,
         3397,
         1625,
+        2011,
         # insert next challenge here
     ]
     if int(challenge_id) == -1:
@@ -4437,6 +4438,10 @@ def main(challenge_id=-1):
     if int(challenge_id) == 1625:
         from source import Generator1625
         tests = Generator1625.generate()
+
+    if int(challenge_id) == 2011:
+        from source import Generator2011
+        tests = Generator2011.generate()
 
     # insert next challenge import here
 
