0edbca85917a797fc6831d63d045091d1306a837
Marius Stuebs <mqs@stuebs.tech>
2025-10-26 02:01:13 +0200

    'Testcase generator for challenge: #2043. Simple Bank System'

--- generate_testcases.py
+++ generate_testcases.py
@@ -886,6 +886,7 @@ def main(challenge_id=-1):
         3461,
         2048,
         1716,
+        2043,
         # insert next challenge here
     ]
     if int(challenge_id) == -1:
@@ -4467,6 +4468,10 @@ def main(challenge_id=-1):
     if int(challenge_id) == 1716:
         from source import Generator1716
         tests = Generator1716.generate()
+
+    if int(challenge_id) == 2043:
+        from source import Generator2043
+        tests = Generator2043.generate()
 
     # insert next challenge import here
 
